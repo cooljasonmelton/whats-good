8b04bf1b12c6cb4d1a133bd8dcd3be0ab16ff2f5
cooljasonmelton <cooljasonmelton>
2020-10-05 18:03:41 -0500

    styling menu

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -6,7 +6,12 @@ import './Menu.css';
 const Menu = () => {
   return (
     <div className="Menu">
-
+      <ul className="cfb">
+        <li>What's Good</li>
+        <li>Library</li>
+        <li>Contact</li>
+        <li>Random</li>
+      </ul>
     </div>
   );
 }
