315685ea12e33d8ebfe02f2441a361a09a969d47
cooljasonmelton <cooljasonmelton>
2020-10-05 17:28:49 -0500

    set up some blog comps

---
+++ src/components/blog/BlogContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// styling
+import './BlogContainer.css';
+
+const BlogContainer = () => {
+  return (
+    <div className="BlogContainer">
+
+    </div>
+  );
+}
+
+export default BlogContainer;
